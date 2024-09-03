5bcc807a745a3f9e27307e31caac3aedb5af1ac0
NMikall <akim_martynov@mail.ru>
2024-09-03 21:39:20 +0300

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <list>
+#include <iterator>
+#include "queue"
+#include "stack"
+#include "deque"
+#include <cmath>
+
+using namespace std;
+
+vector<int> vec(vector<int>& vectors, int n) {
+    for (int j = 0; j < vectors.size(); j++) {
+        vectors[j] += n;
+    }
+    return vectors;
+}
